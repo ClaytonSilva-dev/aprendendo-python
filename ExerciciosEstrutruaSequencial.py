4a4c4adf1cbdcc92018f8c6f4abe840057f290e0
ClaytonSilva-dev <claytonsilva.dev@gmail.com>
2019-09-30 13:19:39 -0300

    exercicio 6

---
+++ ExerciciosEstrutruaSequencial.py
@@ -0,0 +1,71 @@
+'''
+#1) Faça um programa que msotre a mensagem "Alo mundo".
+
+print ("Alo mundo")
+'''
+
+'''
+#2) Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+numero = int(input("Digite um numero: "))
+print("O numero digitado é: ", numero)
+'''
+
+'''
+#3) Faça um Programa que peça dois números e imprima a soma.
+
+primeiro_numero = int(input("Digite o primeiro numero: "))
+segundo_numero = int(input("Digite o segundo numero: "))
+
+soma = primeiro_numero + segundo_numero
+
+print(soma)
+'''
+
+'''
+4)Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+
+nota_primeiro_bimestre = int(input("Digite a nota do primeiro bimestre: "))
+nota_segundo_bimestre = int(input("Digite a nota do segundo bimestre: "))
+nota_terceiro_bimestre = int(input("Digite a nota do terceiro bimestre: "))
+nota_quarto_bimestre = int(input("Digite a nota do quarto bimestre: "))
+
+media = (nota_primeiro_bimestre + nota_segundo_bimestre + nota_terceiro_bimestre + nota_quarto_bimestre)/4
+
+print("A media anual é: ", media)
+'''
+
+'''
+5) Faça um Programa que converta metros para centímetros.
+
+
+metros = int(input("Digite a quantidade em metros: "))
+centimetros = metros*100
+
+print("A medida em cemtimetros é: ", centimetros, "cm")
+'''
+
+'''
+6) Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+'''
+
+raio = int(input("Digite o raio: "))
+area = raio*3.14**2
+print("A area é: ", area, "m2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
